60978fab38d488db8ea1c09b7996412a11e5e41a
Juan Arredondo <juan.arredondo.c@usach.cl>
2023-06-01 19:36:28 -0400

    Retest Angular

---
+++ native_code/Java/logIn.js
@@ -0,0 +1,14 @@
+const btnLogIn = document.querySelector('#logIn');
+const form = document.querySelector('#form');
+const mesaggeWrong = document.querySelector('#wrongPass');
+
+form.addEventListener('submit', submitEvent);
+
+function submitEvent(event){
+    event.preventDefault();
+    const userId = document.querySelector('#userId');
+    const userPassword = document.querySelector('#userPassword');
+    if (userId.value == "demo" && userPassword.value == "1234"){
+            location.href = "../Html/evaluaciones.html";
+    }else { mesaggeWrong.classList.remove('inactive')};
+}
