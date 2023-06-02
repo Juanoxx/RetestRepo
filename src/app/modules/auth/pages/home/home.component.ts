52e231707cb1cd004c05a8b3e08ee385416e14b9
Juan Arredondo <juan.arredondo.c@usach.cl>
2023-06-02 00:41:56 -0400

    fix scroll home page

--- src/app/modules/auth/pages/home/home.component.ts
+++ src/app/modules/auth/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public sections = [
+    { id: 'secction-1', name: 'Descripción' },
+    { id: 'secction-2', name: 'Equipo' },
+    { id: 'secction-3', name: 'Nuestros propósitos' },
+    { id: 'secction-4', name: 'Funcionamiento' },
+    { id: 'secction-5', name: 'Contacto' }
+  ];
+  
+  constructor(private route: ActivatedRoute) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  public scrollTo(elementId: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({behavior: 'smooth'});
+    }
+  }
+  
 }
